1969e1313c8ebd7733c371f756e2f61141b7e88f
Leandro Neves <leandrosoares6@hotmail.com>
2019-04-13 13:01:35 -0300

    Inclusao de usuario via JSON realizado com sucesso

--- src/App.hs
+++ src/App.hs
@@ -7,6 +7,7 @@ module App where
     import Configs.DbConfig
     import Routes.UserApi
     import Server
+    import Models.User
 
 -- Dependencias externas
     import System.IO
@@ -14,6 +15,7 @@ module App where
     import Network.Wai.Handler.Warp
     import Servant
     import Database.PostgreSQL.Simple
+    import Database.PostgreSQL.ORM.Model
     import Data.Pool
     import qualified Data.Configurator as C
 
@@ -27,8 +29,10 @@ module App where
                 putStrLn "Database nao encontrado para as configuracoes especificadas!"
 
             Just conf -> do
-                {- conn <- newConn conf
-                _ <- execute_ conn "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name varchar(45) NOT NULL, email varchar(45) NOT NULL);" -}
+                let usr = User NullKey "leandro" "leandro@example.com"
+                conn <- newConn conf
+                _ <- execute_ conn "CREATE TABLE IF NOT EXISTS \"user\"(id serial primary key, name varchar(45) NOT NULL, email varchar(45) NOT NULL);"
+                _ <- trySave conn usr
                 pool <- createPool (newConn conf) close 1 64 10
                 let port = 4000
                     settings =
@@ -38,4 +42,4 @@ module App where
                 runSettings settings =<< mkApp pool
     
     mkApp :: Pool Connection -> IO Application
-    mkApp conns = return $ (serve userApi $ server conns)
+    mkApp conns = return $ (serve userApi (server conns))
